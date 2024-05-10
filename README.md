a90d961fbbd181ef2a656ed2905e413643240110
Nezort11 <nezort11@gmail.com>
2024-05-10 12:40:03 +0300

    feat: Add password silence, refactor, add example usage

--- README.md
+++ README.md
@@ -5,4 +5,25 @@ npm install -g tglogin
 tglogin
 # or
 npx tglogin
+
+# ⚙️  Please enter API id (got from https://my.telegram.org): ********
+# ⚙️  Please enter API hash (got from https://my.telegram.org): ********************************
+# ⏳ Logging into telegram account...
+# [2024-05-10T12:37:10.003] [INFO] - [Running gramJS version 2.20.14]
+# [2024-05-10T12:37:10.017] [INFO] - [Connecting to 149.154.167.91:80/TCPFull...]
+# [2024-05-10T12:37:11.720] [INFO] - [Connection to 149.154.167.91:80/TCPFull complete!]
+# [2024-05-10T12:37:11.721] [INFO] - [Using LAYER 177 for initial connect]
+# 📞 Please enter your phone number: +79312930238
+# [2024-05-10T12:37:25.567] [INFO] - [Phone migrated to 2]
+# [2024-05-10T12:37:25.617] [INFO] - [Reconnecting to new data center 2]
+# [2024-05-10T12:37:25.672] [WARN] - [Disconnecting...]
+# [2024-05-10T12:37:25.673] [INFO] - [Disconnecting from 149.154.167.91:80/TCPFull...]
+# [2024-05-10T12:37:25.675] [INFO] - [Connecting to 149.154.167.51:80/TCPFull...]
+# [2024-05-10T12:37:25.677] [INFO] - [connection closed]
+# [2024-05-10T12:37:27.287] [INFO] - [Connection to 149.154.167.51:80/TCPFull complete!]
+# [2024-05-10T12:37:27.288] [INFO] - [Using LAYER 177 for initial connect]
+# 💬 Please enter the code you received: 11217
+# [2024-05-10T12:37:37.560] [INFO] - [Signed in successfully as i am i]
+# ✅ Successfully connected. Here is your session id:
+# 1AgAOMTQ5asdgjkkljasdlkjfFi3mRKj5WU7zXRHMlafYt5f5QFMf67QYHO0UYouX8kpN7hpxVVjt/5fFwf3UegLqPackK+JTO1wNNUpfzCrv+pk6askgjaslkdjgkptI1vE4wRdLHpF+vROvpwC4K9OyxQ/4g7/01Y3ZBOp6FOA/IvJq8XaGYn8asdgklaslkdjggrtiIX2TypJ5NwwDEL4N6Xs0VcSPS0ICiSXO8JPQHValAMasdfjkasdkljfBZVa6j/UtqaYmxUPsadgjl1cLVqVQEwe6+rhagskdlj32781+3285IZOaJVtrNA=
 ```
--- bin.js
+++ bin.js
@@ -1,31 +1,47 @@
 #!/usr/bin/env node
-import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
-import { TelegramClient, sessions } from "telegram";
 import process from "process";
-
-const rl = readline.createInterface({ input, output });
+import { read } from "read";
+import { TelegramClient, sessions } from "telegram";
 
 const main = async () => {
-    const apiId = await rl.question("Please enter API id (got from https://my.telegram.org): ");
-    const apiHash = await rl.question("Please enter API hash (got from https://my.telegram.org): ");
+  const apiId = await read({
+    prompt: "⚙️  Please enter API id (got from https://my.telegram.org): ",
+    silent: true,
+    replace: "*",
+  });
 
-    console.log("Logging into telegram account...)");
-    const client = new TelegramClient(new sessions.StringSession(""), +apiId, apiHash, {
-        connectionRetries: 5,
-    });
-    await client.start({
-      phoneNumber: async () => await rl.question("Please enter your phone number: "),
-      password: async () => await rl.question("Please enter your password: "),
-      phoneCode: async () => await rl.question("Please enter the code you received: "),
-      onError: (error) => console.error(error),
-    });
-    rl.close();
+  const apiHash = await read({
+    prompt: "⚙️  Please enter API hash (got from https://my.telegram.org): ",
+    silent: true,
+    replace: "*",
+  });
 
-    console.log("Successfully connected. Here is your session id:");
-    console.log(client.session.save());
-    process.exit(0);
-}
+  console.log("⏳ Logging into telegram account...");
+  const client = new TelegramClient(
+    new sessions.StringSession(""),
+    +apiId,
+    apiHash,
+    {
+      connectionRetries: 5,
+    }
+  );
+  await client.start({
+    phoneNumber: async () =>
+      await read({ prompt: "📞 Please enter your phone number: " }),
+    password: async () =>
+      await read({
+        prompt: "🔒 Please enter your password: ",
+        silent: true,
+        replace: "*",
+      }),
+    phoneCode: async () =>
+      await read({ prompt: "💬 Please enter the code you received: " }),
+    onError: (error) => console.error(error),
+  });
+
+  console.log("✅ Successfully connected. Here is your session id:");
+  console.log(client.session.save());
+  process.exit(0);
+};
 
 main();
-
